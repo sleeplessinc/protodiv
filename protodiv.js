0072e4386388efde5816e1ac97c41a14fb72ad89
Joe Hitchens <joe@sleepless.com>
2011-08-03 12:16:48 -0700

    bug fixes

--- protodiv.js
+++ protodiv.js
@@ -4,6 +4,7 @@
 function ProtoDiv(id) {
 
 	this.proto = document.getElementById(id)
+	this.proto.id = ""
 	this.mommy = this.proto.parentNode 
 	this.mommy.removeChild(this.proto)
 
@@ -11,8 +12,26 @@ function ProtoDiv(id) {
 		this.mommy.innerHTML = ""
 	}
 
+	this.map = function(node, list, cb) {
+		if(node.hasChildNodes()) {
+			var kids = node.childNodes
+			for(var i = 0; i < kids.length; i++) {
+				var kid = kids[i]
+				if(cb(kid))
+					list.push(kid)
+				this.map(kid, list, cb)
+			}
+		}
+	}
+
+	this.reduce = function(list, cb) {
+		var i, l = list.length
+		for(i = 0; i < l; i++) 
+			cb(list[i])
+	}
+
 	this.replicate = function(data) {
-		var dl, i, clone, h, d, re
+		var dl, i, clone, h, d, re, list, c
 
 		dl = data.length
 		for(i = 0; i < dl; i++) {
@@ -34,15 +53,28 @@ function ProtoDiv(id) {
 
 			clone.innerHTML = h
 
-			/*
 			for(key in d) {
+				c = key.substring(1)
+				list = []
 				switch(key.substring(0,1)) {
 				case ".":
+					this.map(clone, list, function(e) {
+						return e.className == c
+					})
+					this.reduce(list, function(e) {
+						e.innerHTML = d[key]
+					})
+					break
 				case "#":
-					$e.find(key).html(d[key])
+					this.map(clone, list, function(e) {
+						return e.id == c
+					})
+					this.reduce(list, function(e) {
+						e.innerHTML = d[key]
+					})
+					break
 				}
 			}
-			*/
 
 			this.mommy.appendChild(clone)
 		}
