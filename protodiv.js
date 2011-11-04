526d057a48698e9e2780a88328af041ca682724a
Joe Hitchens <joe@sleepless.com>
2011-11-04 16:47:15 -0700

    added some var keywords

--- protodiv.js
+++ protodiv.js
@@ -47,7 +47,7 @@ ProtoDiv.map = function(node, list, cb) {
 }
 
 ProtoDiv.substitute = function(s, obj) {
-	for(key in obj) {
+	for(var key in obj) {
 		re = new RegExp("__"+key+"__", "g")
 		s = s.replace(re, obj[key])
 	}
@@ -56,18 +56,17 @@ ProtoDiv.substitute = function(s, obj) {
 
 ProtoDiv.inject = function(id, obj) {
 	var proto = ProtoDiv.elem(id)
-	var i
 
 	proto.innerHTML = ProtoDiv.substitute(proto.innerHTML, obj)
 
-	for(i = 0; i < proto.attributes.length; i++) {
-		a = proto.attributes[i]
+	for(var i = 0; i < proto.attributes.length; i++) {
+		var a = proto.attributes[i]
 		a.textContent = ProtoDiv.substitute(a.textContent, obj)
 	}
 
-	for(key in obj) {
-		c = key.substring(1)
-		list = []
+	for(var key in obj) {
+		var c = key.substring(1)
+		var list = []
 		switch(key.substring(0,1)) {
 		case "#":
 			ProtoDiv.map(proto, list, function(e) {
@@ -105,9 +104,9 @@ ProtoDiv.replicate = function(id, arr, keep) {
 		proto.origDisplay = proto.style.display
 	}
 
-	for(i = 0; i < l; i++) {
+	for(var i = 0; i < l; i++) {
 		obj = arr[i]
-		e = proto.cloneNode(true)
+		var e = proto.cloneNode(true)
 		delete e.id
 		mom.insertBefore(e, sib)
 		ProtoDiv.inject(e, obj)
